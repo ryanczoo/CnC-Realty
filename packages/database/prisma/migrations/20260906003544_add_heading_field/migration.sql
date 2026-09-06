16f914b755e8edae170c0dac731b4d3364176e62
Zoo <aznfrkguy@gmail.com>
2026-09-05 17:36:22 -0700

    feat(db): add optional heading field to Campaign, ActionPlanStep, LeadPlanStep, DripStep

---
+++ packages/database/prisma/migrations/20260906003544_add_heading_field/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "ActionPlanStep" ADD COLUMN     "heading" TEXT;
+
+-- AlterTable
+ALTER TABLE "Campaign" ADD COLUMN     "heading" TEXT;
+
+-- AlterTable
+ALTER TABLE "DripStep" ADD COLUMN     "heading" TEXT;
+
+-- AlterTable
+ALTER TABLE "LeadPlanStep" ADD COLUMN     "heading" TEXT;
